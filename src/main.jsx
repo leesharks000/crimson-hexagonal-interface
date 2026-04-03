3ac276944554816b33d9cb6fca149d5d5b5e8866
leesharks000 <leesharks00@gmail.com>
2026-04-03 17:11:37 -0400

    fix: remove missing css import and use explicit jsx entry import

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import HexagonInterfaceFixed from './HexagonInterfaceFixed'
-import './index.css'
+import HexagonInterfaceFixed from './HexagonInterfaceFixed.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <HexagonInterfaceFixed />
-  </React.StrictMode>,
+  </React.StrictMode>
 )
