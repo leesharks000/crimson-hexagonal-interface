e3caff4eea6b9963b216a4ecc01653ea5bae87c5
leesharks000 <leesharks00@gmail.com>
2026-04-03 16:41:29 -0400

    fix: route app entrypoint to normalized interface component

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import HexagonInterface from './HexagonInterface.jsx'
+import HexagonInterfaceFixed from './HexagonInterfaceFixed'
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HexagonInterface />
-  </React.StrictMode>
+    <HexagonInterfaceFixed />
+  </React.StrictMode>,
 )
